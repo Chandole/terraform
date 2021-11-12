01c325255bcd332e6a0fa8374021aabfb24b9cac
shubham <shubhamchandole@gmail.com>
2021-11-12 14:58:25 +0530

    --- Auto-Git Commit ---

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 provider "aws" {
-  # ... potentially other provider configuration ...
 
-  endpoints {
-    dynamodb = "http://localhost:4569"
-    s3       = "http://localhost:4572"
-  }
+region = 
 }
