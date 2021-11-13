4a3ec7a124aaa38f18e386fb4d0b8dee895c57cd
shubham <shubhamchandole@gmail.com>
2021-11-13 20:37:51 +0530

    --- Auto-Git Commit ---

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ resource "aws_instance" "webserver" {
 resource "aws_volume_attachment" "ebs_att" {
  device_name = "/dev/sdd"
  volume_id = aws_instance.webserver.id
- #  force_detach = true
-#} 
+  force_detach = fa
+} 
 
 output "private_ip" {
     value = aws_instance.webserver.private_ip  
