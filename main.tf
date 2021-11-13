c92cb9fbc1535aab3db93d4bac5c8c1e67b56376
shubham <shubhamchandole@gmail.com>
2021-11-13 13:04:15 +0530

    --- Auto-Git Commit ---

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "webserver" {
+    ami = "ami-0b0af3577fe5e3532"
+    instance_type = "t2.micro"
+    availability_zone ="us-east-1b"
+    tags = var.tags
+
+}
