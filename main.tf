333e1d5a8fbd1f0591222e9c6c5ef93f3a00fe43
shubham <shubhamchandole@gmail.com>
2021-11-13 19:42:58 +0530

    --- Auto-Git Commit ---

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "webserver" {
+    ami = var.ami
+    instance_type = var.instance_type
+    availability_zone = var.availability_zone
+    tags = var.tags
+}
+
+output "private_ip" {
+    value = aws_instance.webserver.private_ip  
+}
