e8882d771f585a5f87f112e9da246928d8ebe247
shubham <shubhamchandole@gmail.com>
2021-11-14 14:54:09 +0530

    --- Auto-Git Commit ---

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "webserver" {
     
 }
 
-resource "aws_volume_attachment" "ebs_attach" {
+resource "aws_volume" "ebs_attach" {
  device_name = "/dev/sdd"
  instance_id = aws_instance.webserver.id
  volume_id = aws_instance.webserver.id
