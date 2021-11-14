944487e746ad05a8e5c16bb377c3b9029e830879
shubham <shubhamchandole@gmail.com>
2021-11-14 12:57:27 +0530

    --- Auto-Git Commit ---

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "webserver" {
 
 resource "aws_volume_attachment" {
   type = string
- device_name = "/dev/sdd"
+ device_name = var.de
  instance_id = aws_instance.webserver.id
  volume_id = aws_instance.webserver.id
   force_detach = true
