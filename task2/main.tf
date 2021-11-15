8ba5fa524af95e838a29d56881c24efa44c1f1d7
shubham <shubhamchandole@gmail.com>
2021-11-15 19:16:49 +0530

    --- Auto-Git Commit ---

--- task2/main.tf
+++ task2/main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "SG-test" {
       cidr_blocks = [ "0.0.0.0/0" ]
       description = "value"
       from_port = 0
-      ipv6_cidr_blocks = [ "value" ]
+      ipv6_cidr_blocks = [ "::/0" ]
       prefix_list_ids = [ "value" ]
       protocol = "value"
       security_groups = [ "value" ]
