c67e1d3ae44cfdb6eac3d9da689d042fe5c508f1
shubham <shubhamchandole@gmail.com>
2021-11-15 19:54:57 +0530

    --- Auto-Git Commit ---

--- task2/var.tf
+++ task2/var.tf
@@ -23,7 +23,7 @@ variable "ingress" {
      description = "Allow SG-HTTP"
       cidr_blocks = [ "0.0.0.0/0" ]
       from_port = "80"
-      to_port = 80
+      to_port = "80"
       protocol = "tcp"  
   
 }
