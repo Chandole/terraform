333504750038c6a1a1bbbd552e6f844f5ab5a159
shubham <shubhamchandole@gmail.com>
2021-11-15 19:42:42 +0530

    --- Auto-Git Commit ---

--- task2/main.tf
+++ task2/main.tf
@@ -43,13 +43,12 @@ resource "aws_security_group" "SG-test" {
     } ]
     
     egress = [ {
-        description = "value"
+      description = "value"
       cidr_blocks = [ "0.0.0.0/0" ]
       from_port = 0
       to_port = 0
       ipv6_cidr_blocks = [ "::/0" ]
-     # prefix_list_ids = [ "value" ]
-      protocol = "-1"
+        protocol = "-1"
     } ]
      tags = {
     Name = "allow_tls"
