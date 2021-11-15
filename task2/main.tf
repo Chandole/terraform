7a0edc50a8180e4c9ee0f1ff4e347dd76f0dc8b4
shubham <shubhamchandole@gmail.com>
2021-11-15 18:48:28 +0530

    --- Auto-Git Commit ---

---
+++ task2/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region ="us-east-1"
+
+}
+
+resource "aws_instance" "test" {
+ami = var.ami
+instance_type = var.instance_type
+availability_zone = var.availability_zone
+tags = var.tags
+}
+resource "aws_ebs_volume" "Vol-1" {
+    availability_zone = var.availability_zone
+    size = 10
+    tags = {
+      "Name" = "Ebs-1"
+      "ENV" = "DEV"
+    }
+  
+}
