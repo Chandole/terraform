bdb5302e8e0168174b9fa1be1f455960f8308ec7
shubham <shubhamchandole@gmail.com>
2021-11-15 19:49:36 +0530

    --- Auto-Git Commit ---

---
+++ task2/var.tf
@@ -0,0 +1,38 @@
+variable "ami" {
+type = string
+default = "ami-0b0af3577fe5e3532"
+}
+variable "instance_type" {
+  
+  type = string
+  default = "t2.micro"
+}
+variable "availability_zone" {
+  type = string
+  default = "us-east-1b"
+  
+}
+variable "tags" {
+type = map(string)
+default = {
+  Name = "Test"
+  Env = "Dev"
+}  
+}
+variable "ingress" {
+     description = "Allow SG-HTTP"
+      cidr_blocks = [ "0.0.0.0/0" ]
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"  
+  
+}
+
+    variable "egress" {
+      description = "OUT-BOND"
+      cidr_blocks = [ "0.0.0.0/0" ]
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      
+    }
