c39a1384d60c2a9e5241cacd0682c7979111ede1
shubham <shubhamchandole@gmail.com>
2021-11-15 18:45:27 +0530

    --- Auto-Git Commit ---

--- task2/var.tf
+++ task2/var.tf
@@ -7,3 +7,15 @@ variable "instance_type" {
   type = string
   default = "t2.micro"
 }
+variable "availability_zone" {
+  type = string
+  default = "us-east-1b"
+  
+}
+variable "tags" {
+type = map(string)
+default = {
+  Name = "Instance-test"
+  Env = "Dev"
+}  
+}
