0892d01e236346be5a7fb9b71a8d720cfc4d967e
shubham <shubhamchandole@gmail.com>
2021-11-23 14:20:55 +0530

    --- Auto-Git Commit ---

--- task1/main.tf
+++ task1/main.tf
@@ -44,6 +44,17 @@ name = "shubham"
 resource "aws_iam_group" "developers" {
   name = "developers" 
 }
+#Iam User added in group
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.user_one.name,
+    aws_iam_user.user_two.name,
+  ]
+
+  group = aws_iam_group.group.name
+}
 
 
 output "private_ip" {
